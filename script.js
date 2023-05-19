b8953ea2f7abdf936e7c2a4cb6a1eab4710e3430
rventurino <adventurino@outlook.com>
2023-05-19 13:37:35 -0400

    preliminary used records page

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+//Highlight the current page in the nav bar, css located in headernav.css
+
+function setCurrentNavPage () {
+// Get the current URL path
+var path = window.location.pathname;
+
+// Get all the anchor elements in the navbar
+const newRecordsNav = document.getElementById('newRecordsNav');
+var navLinks = document.querySelectorAll('#navbar a');
+// Loop through the anchor elements
+for (var i = 0; i < navLinks.length; i++) {
+  var href = navLinks[i].getAttribute('href');
+  //console.log("href: " + href)
+  //console.log("path: " + path)
+  // Check if the href attribute matches the current URL path
+  if (path === href) {
+    // Add a CSS class to highlight the current navbar element
+    navLinks[i].classList.add('current');
+    break; // Stop the loop since we found a match
+  }
+  else if(path === "/new-records.html"){
+    newRecordsNav.classList.add('current');
+  }
+  else if(path === "/new-releases.html"){
+    newRecordsNav.classList.add('current');
+  }
+}
+}
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbnZlbnR1cmlubyIsImEiOiJjbGdvMXRmaXQwM2NoM2RzOHBtdHRpa2ZnIn0.TD2RDUFvN4mQEHTKj2f-Qw';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -58,47 +87,4 @@ newRecordsNav.addEventListener('mouseover', () => {
     caretnew.style.color = 'black';
   });
 
-//Highlight the current page in the nav bar, css located in headernav.css
 
-function setCurrentNavPage () {
-  /*
-  const current = 0;
-  const navbar = document.getElementById('navbar');
-  var url = location.href.split('/');
-  var url2 = url[url.length - 1]
-
-  for (var i = 0; i < 20; i++) {
-    //console.log("i: " + i)
-    //console.log("url2: " + url2)
-    console.log(navbar.children[i].children[1])
-    if (navbar.children[i].href === url2) {
-        current = i;
-        console.log("ifstatement")
-    }
-}
-//console.log(current)
-navbar.children[current].classList.add('current');
-*/
-// Get the current URL path
-var path = window.location.pathname;
-//console.log("pathtop: " + path)
-
-// Get all the anchor elements in the navbar
-var navLinks = document.querySelectorAll('#navbar a');
-for (var index = 0; index < navLinks.length; index++) {
-  console.log(navLinks[index]);
-}
-
-// Loop through the anchor elements
-for (var i = 0; i < navLinks.length; i++) {
-  var href = navLinks[i].getAttribute('href');
-  //console.log("href: " + href)
-  //console.log("path: " + path)
-  // Check if the href attribute matches the current URL path
-  if (path === href) {
-    // Add a CSS class to highlight the current navbar element
-    navLinks[i].classList.add('current');
-    break; // Stop the loop since we found a match
-  }
-}
-}
