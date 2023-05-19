81225d63d45ce8d495c25c73aba8bee0250d2f65
Ryan <ryan.venturino@gmail.com>
2023-05-18 20:02:42 -0400

    navbar JS (Working!!)

--- script.js
+++ script.js
@@ -60,18 +60,45 @@ newRecordsNav.addEventListener('mouseover', () => {
 
 //Highlight the current page in the nav bar, css located in headernav.css
 
+function setCurrentNavPage () {
+  /*
+  const current = 0;
+  const navbar = document.getElementById('navbar');
+  var url = location.href.split('/');
+  var url2 = url[url.length - 1]
 
-
-const current = 0;
-const navbar = document.getElementById('navbar');
-var url = location.href.split('/');
-var url2 = url[url.length - 1]
-
-  for (var i = 0; i < navbar.children.length; i++) {
+  for (var i = 0; i < 20; i++) {
+    //console.log("i: " + i)
+    //console.log("url2: " + url2)
+    console.log(navbar.children[i].children[1])
     if (navbar.children[i].href === url2) {
         current = i;
+        console.log("ifstatement")
     }
 }
+//console.log(current)
 navbar.children[current].classList.add('current');
+*/
+// Get the current URL path
+var path = window.location.pathname;
+//console.log("pathtop: " + path)
 
+// Get all the anchor elements in the navbar
+var navLinks = document.querySelectorAll('#navbar a');
+for (var index = 0; index < navLinks.length; index++) {
+  console.log(navLinks[index]);
+}
 
+// Loop through the anchor elements
+for (var i = 0; i < navLinks.length; i++) {
+  var href = navLinks[i].getAttribute('href');
+  //console.log("href: " + href)
+  //console.log("path: " + path)
+  // Check if the href attribute matches the current URL path
+  if (path === href) {
+    // Add a CSS class to highlight the current navbar element
+    navLinks[i].classList.add('current');
+    break; // Stop the loop since we found a match
+  }
+}
+}
