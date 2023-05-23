9b016ab990c6c886624ba6550a3a82e68c93d93b
rventurino <adventurino@outlook.com>
2023-05-23 16:39:50 -0400

    nav improvements and contacts page

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var path = window.location.pathname;
 
 // Get all the anchor elements in the navbar
 const newRecordsNav = document.getElementById('newRecordsNav');
+const usedRecordsNav = document.getElementById('usedRecordsNav')
+const usedRecordsCaret = document.getElementById('caretUsed')
 var navLinks = document.querySelectorAll('#navbar a');
 // Loop through the anchor elements
 for (var i = 0; i < navLinks.length; i++) {
@@ -20,27 +22,18 @@ for (var i = 0; i < navLinks.length; i++) {
   }
   else if(path === "/new-records.html"){
     newRecordsNav.classList.add('current');
+
   }
   else if(path === "/new-releases.html"){
     newRecordsNav.classList.add('current');
+  }else if(path === "/used-records.html"){
+    usedRecordsNav.classList.add('current');
+    usedRecordsCaret.classList.add('currentcaret')
   }
 }
 }
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbnZlbnR1cmlubyIsImEiOiJjbGdvMXRmaXQwM2NoM2RzOHBtdHRpa2ZnIn0.TD2RDUFvN4mQEHTKj2f-Qw';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/ryanventurino/clgo2uqq3007y01nyecqhexqa', // style URL
-    center: [-79.9747218887018, 40.48069876156457, ], // starting position [lng, lat]
-    zoom: 17 // starting zoom
-});
 
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-
-//Add full screen control
-map.addControl(new mapboxgl.FullscreenControl());
 
 //nav bar controls 
 
@@ -87,4 +80,19 @@ newRecordsNav.addEventListener('mouseover', () => {
     caretnew.style.color = 'black';
   });
 
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbnZlbnR1cmlubyIsImEiOiJjbGdvMXRmaXQwM2NoM2RzOHBtdHRpa2ZnIn0.TD2RDUFvN4mQEHTKj2f-Qw';
+  const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/ryanventurino/clgo2uqq3007y01nyecqhexqa', // style URL
+      center: [-79.9747218887018, 40.48069876156457, ], // starting position [lng, lat]
+      zoom: 17 // starting zoom
+  });
+  
+  // Add zoom and rotation controls to the map.
+  map.addControl(new mapboxgl.NavigationControl());
+  
+  //Add full screen control
+  map.addControl(new mapboxgl.FullscreenControl());
 
